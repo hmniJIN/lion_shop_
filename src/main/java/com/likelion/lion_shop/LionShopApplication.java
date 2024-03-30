10385b4f511a51a1904050787ea13ef7d6621026
hmniJIN <hmni.JIN@gmail.com>
2024-03-30 18:35:17 +0900

    1주차 과제 제출

---
+++ src/main/java/com/likelion/lion_shop/LionShopApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.lion_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LionShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LionShopApplication.class, args);
+	}
+
+}
