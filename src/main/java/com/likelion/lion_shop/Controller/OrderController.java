1bc9d483f3492ff2056560ce1d65e8191ea66ccc
600gramSik <geunsikevin@naver.com>
2024-04-03 01:09:36 +0900

    1주차 과제 나갑니다

---
+++ src/main/java/com/likelion/lion_shop/Controller/OrderController.java
@@ -0,0 +1,15 @@
+package com.likelion.lion_shop.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/order") // uri가 http://~/user로 시작하는 요청을 받습니다.
+public class OrderController {
+
+    // 1. 주문을 생성하는 컨트롤러를 만듭니다. 이때 return 값은 "주문 생성하기"입니다. -> 주문은 리스트 형태로 요청을 보내주세요!
+    // 2. 주문을 가져오는 컨트롤러를 만듭니다. 이때 return 값은 "주문 가져오기"입니다.
+    // 3. 주문을 수정하는 컨트롤러를 만듭니다. 이때 return 값은 "주문 수정하기"입니다.
+    // 4. 주문을 삭제하는 컨트롤러를 만듭니다. 이때 return 값은 "주문 삭제하기"입니다.
+
+}
