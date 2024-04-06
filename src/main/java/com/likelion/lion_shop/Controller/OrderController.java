424ed2289a092fcf71914869e7004e404475d5f1
hmniJIN <hmni.JIN@gmail.com>
2024-04-06 20:05:09 +0900

    1주차 과제 제출

--- src/main/java/com/likelion/lion_shop/Controller/OrderController.java
+++ src/main/java/com/likelion/lion_shop/Controller/OrderController.java
@@ -1,15 +1,35 @@
 package com.likelion.lion_shop.Controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+//추가
+import com.likelion.lion_shop.Dto.CreateOrderRequestDto;
+import com.likelion.lion_shop.Dto.UpdateOrderRequestDto;
+import java.util.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/order") // uri가 http://~/user로 시작하는 요청을 받습니다.
 public class OrderController {
-
     // 1. 주문을 생성하는 컨트롤러를 만듭니다. 이때 return 값은 "주문 생성하기"입니다. -> 주문은 리스트 형태로 요청을 보내주세요!
+    @PostMapping("/create")
+    public String createOrder(@RequestBody List<CreateOrderRequestDto> CreateOrderlist){
+        return "주문 생성하기";
+    }
     // 2. 주문을 가져오는 컨트롤러를 만듭니다. 이때 return 값은 "주문 가져오기"입니다.
+    @GetMapping("/{userID}")
+    public String getOrder(@PathVariable long userID){
+        return "주문 가져오기";
+    }
     // 3. 주문을 수정하는 컨트롤러를 만듭니다. 이때 return 값은 "주문 수정하기"입니다.
+    @PutMapping("/{userID}")
+    public String updateOrder(@PathVariable long userID, @RequestBody UpdateOrderRequestDto updateOrderRequestDto){
+        return "주문 수정하기";
+    }
     // 4. 주문을 삭제하는 컨트롤러를 만듭니다. 이때 return 값은 "주문 삭제하기"입니다.
+    @DeleteMapping("/{userID}")
+    public String deleteOrder(@PathVariable long userID){
+        return "주문 삭제하기";
+    }
 
 }
